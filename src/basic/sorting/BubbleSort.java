f7574852616f7e18df5e978f8f42960d4a508dd8
hipradeep <pradeepmaurya.cse@gmail.com>
2022-03-18 11:18:02 +0530

    bubble sort

--- src/basic/sorting/BubbleSort.java
+++ src/basic/sorting/BubbleSort.java
@@ -5,24 +5,71 @@ public class BubbleSort {
     //also known as sinking sort, exchange sort
     // with each iteration the largest element goes at end
     public static void main(String[] args) {
-        int[] arr={4, 3, 1, 2, 5};
-        int n= arr.length;
+        // int[] arr={4, 3, 1, 2, 5};
+        //int[] arr={1, 1, 1, 1, 1};
+        int[] arr = {5, 4, 3, 2, 1};
+        // int[] arr={1, 2, 3, 4, 5};
+        //int[] arr={1};
+
+        int n = arr.length;
+        bubbleSortOpt(arr, n);
+    }
+
+    //optimise solution
+    private static void bubbleSortOpt(int[] arr, int n) {
         printArr(arr);
-        for (int i=0; i<n; i++){
-            for (int j=0; j<n-1; j++){
-                if(arr[j]>arr[j+1]){
-                    int temp=arr[j];
-                    arr[j]=arr[j+1];
-                    arr[j+1]=temp;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    swap(arr, j);
                 }
             }
         }
         printArr(arr);
+        /*
+         * Optimization for bubble sort , why we iterate loop for those elements which are already sorted
+         * in second loop 'j<n-1-i' exclude that elements that are already sorted at end
+         * */
     }
-    static void printArr(int[] array){
-        for (int a: array ) {
-            System.out.print(a+", ");
+
+
+    //non optimise solution
+    private static void bubbleSort(int[] arr, int n) {
+        printArr(arr); //before sorting
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    swap(arr, j);
+                }
+            }
+        }
+        printArr(arr); // after sorting
+    }
+
+    //swap elements
+    private static void swap(int[] arr, int j) {
+        int temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+    }
+
+
+    //print array
+    static void printArr(int[] array) {
+        for (int a : array) {
+            System.out.print(a + ", ");
         }
         System.out.println();
     }
 }
+
+
+/*
+------------------------------------------------------------------------------------------------
+        #Time complexity
+
+                 Best               Average                Worst
+
+                 Ω(n)               θ(n^2)                 θ(n^2)
+
+ */
---
+++ src/basic/sorting/SelectionSort.java
@@ -0,0 +1,67 @@
+package basic.sorting;
+
+public class SelectionSort {
+
+    //put the right element at right position
+
+    public static void main(String[] args) {
+        int[] arr = {4, 3, 1, 2, 5};
+        //int[] arr={1, 1, 1, 1, 1};
+        //int[] arr = {5, 4, 3, 2, 1};
+        // int[] arr={1, 2, 3, 4, 5};
+        //int[] arr={1};
+
+        int n = arr.length;
+        selectionSort(arr, n);
+    }
+
+
+    private static void selectionSort(int[] arr, int n) {
+        printArr(arr);
+        for (int i = 0; i < n; i++) {
+            int min = i;
+            for (int j = i + 1; j < n - 1; j++) {
+                if (arr[min] > arr[j])
+                    min = j;
+            }
+            int temp = arr[min];
+            arr[min] = arr[i];
+            arr[i] = temp;
+        }
+        printArr(arr);
+
+        /*
+        * first find the min element index then replace it with first loop initial value
+        * second loop help us find the 'min' index
+        * after find the min index then swap elements*/
+
+    }
+
+
+    //swap elements
+    private static void swap(int[] arr, int j) {
+        int temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+    }
+
+
+    //print array
+    static void printArr(int[] array) {
+        for (int a : array) {
+            System.out.print(a + ", ");
+        }
+        System.out.println();
+    }
+}
+
+
+/*
+------------------------------------------------------------------------------------------------
+        #Time complexity
+
+                 Best               Average                Worst
+
+                  Ω(n^2)              θ(n^2)                  θ(n^2)
+
+ */
