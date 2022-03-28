ea23c3f23f7d2fa42893cf43f04002984180496a
hipradeep <pradeepmaurya.cse@gmail.com>
2022-03-28 21:11:44 +0530

    mar28Lab
    Signed-off-by: hipradeep <pradeepmaurya.cse@gmail.com>

--- src/assignment/mar28SEvaLab2/Q6ReverseString.java
+++ src/assignment/mar28SEvaLab2/Q6ReverseString.java
@@ -7,6 +7,25 @@ public class Q6ReverseString {
     //Q 6 wap to reverse given string without using any reverse method
 
     public static void main(String[] args) {
+        String str;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string : ");
 
+        str = sc.nextLine();
+
+        char[] arr = str.toCharArray();
+        int i, j, l = arr.length;
+
+        for (i = 0, j = l - 1; i < j; i++, j--) {
+            char t = arr[i];
+            arr[i] = arr[j];
+            arr[j] = t;
+        }
+        StringBuilder sb = new StringBuilder();
+        for (char a : arr) {
+            sb.append(a);
+        }
+
+        System.out.println(sb);
     }
 }
