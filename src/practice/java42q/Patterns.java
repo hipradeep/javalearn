8f598526ccbdd3b65129b46582214f8462639a95
hipradeep <pradeepmaurya.cse@gmail.com>
2022-04-19 23:55:53 +0530

    19apr
    Signed-off-by: hipradeep <pradeepmaurya.cse@gmail.com>

--- src/practice/java42q/Patterns.java
+++ src/practice/java42q/Patterns.java
@@ -48,13 +48,5 @@ public class Patterns {
         }
     }
 
-    private static void a_pattern() {
-        System.out.println("Pattern A");
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 4; j++) {
-                System.out.print(" *");
-            }
-            System.out.println();
-        }
-    }
+
 }
