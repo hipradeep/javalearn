1a773f1c5eb3bbeb0ac6fd2fee212cbf66889ebb
hipradeep <pradeepmaurya.cse@gmail.com>
2022-04-25 16:08:19 +0530

    25apr
    Signed-off-by: hipradeep <pradeepmaurya.cse@gmail.com>

---
+++ src/dacclass/apr25/LocalClassExample.java
@@ -0,0 +1,49 @@
+package dacclass.apr25;
+
+public class LocalClassExample {
+    String name;
+    int Id;
+
+    public void koyla(){
+
+        class LocalClassA{
+            String address;
+            void print11() {
+                System.out.println("print from local class A");
+            }
+        }
+
+        LocalClassA a=new LocalClassA();
+        a.print11();
+    }
+    public void gold(){
+
+        class LocalClassB implements LolInterface{
+            String address;
+            void print12() {
+                System.out.println("print from local class B");
+            }
+
+            @Override
+            public void hi() {
+                System.out.println("print from local class B, it is implemented method");
+            }
+        }
+
+        LocalClassB b=new LocalClassB();
+        b.print12();
+        b.hi();
+    }
+
+    public static void main(String[] args) {
+         LocalClassExample localClassExample=new LocalClassExample();
+         localClassExample.koyla();
+         localClassExample.gold();
+    }
+
+
+}
+
+interface LolInterface{
+    public void hi();
+}
