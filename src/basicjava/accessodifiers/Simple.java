497d7703e62aa8e348b982ab40d616b231c996cd
hipradeep <pradeepmaurya.cse@gmail.com>
2022-06-17 21:03:33 +0530

    access modifiers
    Signed-off-by: hipradeep <pradeepmaurya.cse@gmail.com>

---
+++ src/basicjava/accessodifiers/Simple.java
@@ -0,0 +1,24 @@
+package basicjava.accessodifiers;
+
+public class Simple extends Abc {
+
+    public static void main(String[] args) {
+        Simple simple=new Simple();
+        String name= simple.name;
+       // int age= simple.age;
+        int roll= simple.roll;
+        int pin= simple.pin;
+
+        Abc abc=new Abc();
+        String name1= abc.name;
+        // int age1= abc.age;
+        int roll1= abc.roll;
+        int pin1= abc.pin;
+    }
+}
+class Abc{
+    String name = "pradeep";
+    private int age = 25;
+    public int roll = 12012;
+    protected int pin = 226021;
+}
