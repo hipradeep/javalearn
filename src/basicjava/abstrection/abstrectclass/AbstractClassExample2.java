d0d0210c363ce43ea23bb0153d1219845b1cdfb6
hipradeep <pradeepmaurya.cse@gmail.com>
2022-06-18 12:53:28 +0530

    abstraction
    Signed-off-by: hipradeep <pradeepmaurya.cse@gmail.com>

---
+++ src/basicjava/abstrection/abstrectclass/AbstractClassExample2.java
@@ -0,0 +1,14 @@
+package basicjava.abstrection.abstrectclass;
+
+public class AbstractClassExample2 {
+    public static void main(String[] args) {
+
+    }
+}
+abstract class House{
+
+    abstract void calculateSize(int area);
+
+
+}
+
