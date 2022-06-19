069cda60e2fd679805144ae855c8f2dfea6dc579
hipradeep <pradeepmaurya.cse@gmail.com>
2022-06-19 23:23:15 +0530

    array
    Signed-off-by: hipradeep <pradeepmaurya.cse@gmail.com>

--- src/Hello.java
+++ src/Hello.java
@@ -1,10 +1,3 @@
-import java.util.Arrays;
-import java.util.Random;
-import java.util.Scanner;
-
 public class Hello {
 
-        public static   void main(String[] args) {
-        System.out.println("Pradeep");
-    }
 }
