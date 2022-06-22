28a97a3342b846d04929eeefafabb3f0f812b6fe
hipradeep <pradeepmaurya.cse@gmail.com>
2022-06-22 23:04:05 +0530

    22june
    Signed-off-by: hipradeep <pradeepmaurya.cse@gmail.com>

--- src/dsanotes/basic/arrays/MergeTwoArrays.java
+++ src/dsanotes/basic/arrays/MergeTwoArrays.java
@@ -8,6 +8,11 @@ public class MergeTwoArrays {
         int[] b = {};
         int m = a.length;
         int n = b.length;
+        mergeTwoArray1(a, b, m, n);
+    }
+
+    private static void mergeTwoArray1(int[] a, int[] b, int m, int n) {
+
         int[] c = new int[m + n];
         int i = 0, j = 0, k = 0;
 
@@ -16,12 +21,13 @@ public class MergeTwoArrays {
                 c[k++] = a[i++];
             else c[k++] = b[j++];
         }
-        for (; i < m; i++) {
+        for ( ; i < m; i++) {
             c[k++] = a[i];
         }
         for (; j < n; j++) {
             c[k++] = b[j];
         }
+
         System.out.println(Arrays.toString(c));
     }
 }
