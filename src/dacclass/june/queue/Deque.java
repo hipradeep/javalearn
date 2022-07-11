0803391d45dbf23595470bab86e762e129be60d8
pradeep <pradeepmaurya.cse@gmail.com>
2022-07-11 22:18:56 +0530

    circular queue
    Signed-off-by: pradeep <pradeepmaurya.cse@gmail.com>

--- src/dacclass/june/queue/Deque.java
+++ src/dacclass/june/queue/Deque.java
@@ -12,13 +12,14 @@ public class Deque {
         rear = -1;
     }
 
+    //insert at front
     void enQueue_front(int item){
         if (front==0 && rear==size-1){
             System.out.println("Queue is full!");
             return;
         }
         if (front==rear+1){
-            System.out.println("Queue is full$!");
+            System.out.println("Queue is full!");
             return;
         }
         //if empty
@@ -35,6 +36,66 @@ public class Deque {
         queue[front]=item;
     }
 
+    // insert at rear
+    void enQueue_rear(int item){
+        if (front==0 && rear==size-1){
+            System.out.println("Q is full");
+            return;
+        }
+        if (front==rear+1){
+            System.out.println("Q is Full");
+            return;
+        }
+        if (front==-1 & rear==-1){
+            front=rear=0;
+            queue[rear]=item;
+            return;
+        }
+        rear++;
+        queue[rear]=item;
+
+    }
+
+    //delete from front
+    int deQueue_front(){
+
+        if (front==-1){
+            System.out.println("Q is Empty!");
+            return -1;
+        }
+        int t=queue[front];
+        if (front==rear){
+            front=rear=-1;
+            return t;
+        }
+        if (front==size-1  ){
+            front=0;
+        }else {
+            front=front+1;
+        }
+
+        return t;
+    }
+
+    //delete from rear
+    int deQueue_rear(){
+        if (front==-1){
+            System.out.println("Q is Empty!");
+            return -1;
+        }
+        int t=queue[rear];
+        if (front==rear){
+            front=rear=-1;
+            return t;
+        }
+        if (rear==0 ){
+            rear=size-1;
+        }else {
+            rear=rear-1;
+        }
+
+        return t;
+    }
 
     void display(){
         if (front==-1){
@@ -49,23 +110,31 @@ public class Deque {
 
     public static void main(String[] args) {
         Deque d=new Deque(5);
-        d.enQueue_front(10);
-        System.out.println("f - "+d.front);
-        System.out.println("r - "+d.rear);
+        d.enQueue_rear(10);
         d.enQueue_front(20);
-        System.out.println("f - "+d.front);
-        System.out.println("r - "+d.rear);
-        d.enQueue_front(30);
-        System.out.println("f - "+d.front);
-        System.out.println("r - "+d.rear);
-        d.enQueue_front(40);
-        System.out.println("f - "+d.front);
-        System.out.println("r - "+d.rear);
-        d.enQueue_front(50);
-        System.out.println("f - "+d.front);
-        System.out.println("r - "+d.rear);
-
+        d.enQueue_rear(30);
+        d.enQueue_rear(40);
+        d.enQueue_rear(50);
+        d.enQueue_rear(60);
+        d.display();
+        System.out.println(d.deQueue_rear());
+        System.out.println(d.deQueue_front());
+        System.out.println(d.deQueue_front());
+        System.out.println(d.deQueue_rear());
+        System.out.println(d.deQueue_rear());
         d.display();
     }
 
 }
+
+/*
+* Deque
+* - Insertion At front end
+* - insertion at rear end
+* - deletion at front end
+* - deletion at rear end
+*
+*
+*
+*
+* */
