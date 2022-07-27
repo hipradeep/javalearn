9a5e1d668e64af940b6a9111ffae0c2e8abcce80
pradeep <pradeepmaurya.cse@gmail.com>
2022-07-28 00:52:57 +0530

    Soring algo - Heap Quick sort
    Signed-off-by: pradeep <pradeepmaurya.cse@gmail.com>

---
+++ src/assignment/jul27/Q3SortingAlgos.java
@@ -0,0 +1,138 @@
+package assignment.jul27;
+
+import java.util.Arrays;
+
+public class Q3SortingAlgos {
+
+    public static void main(String[] args) {
+        int[] arr = {1, 20, 14, 5, 12, 14, 45, 7};
+
+        System.out.println("Insertion sort");
+        insertionSort(arr, arr.length);
+        System.out.println("Bubble Sort");
+        bubbleSort(arr, arr.length);
+        System.out.println("Selection Sort");
+        selectionSort(arr, arr.length);
+        System.out.println("Heap Sort");
+        heapSort(arr);
+        System.out.println("Quick Sort");
+        quickSort(arr, 0, arr.length - 1);
+
+    }
+
+    private static void selectionSort(int[] arr, int n) {
+
+        for (int i = 0; i < n; i++) {
+            int min = i;
+            for (int j = i + 1; j < n - 1; j++) {
+                if (arr[min] > arr[j])
+                    min = j;
+            }
+            int temp = arr[min];
+            arr[min] = arr[i];
+            arr[i] = temp;
+        }
+        System.out.println(Arrays.toString(arr));
+
+    }
+
+    private static void insertionSort(int[] arr, int n) {
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j > 0; j--) {
+                if (arr[j - 1] > arr[j]) {
+                    swap(arr, j, j - 1);
+                }
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+
+    private static void bubbleSort(int[] arr, int n) {
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    //swap(arr, j);
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+        //printArr(arr); // after sorting
+    }
+
+    public static void heapSort(int arr[]) {
+        int n = arr.length;
+
+        for (int i = n / 2 - 1; i >= 0; i--)
+            heapify(arr, n, i);
+        for (int i = n - 1; i > 0; i--) {
+
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+
+            heapify(arr, i, 0);
+        }
+
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void heapify(int arr[], int n, int i) {
+        int largest = i;
+        int l = 2 * i + 1;
+        int r = 2 * i + 2;
+
+        if (l < n && arr[l] > arr[largest])
+            largest = l;
+
+        if (r < n && arr[r] > arr[largest])
+            largest = r;
+
+
+        if (largest != i) {
+            int swap = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = swap;
+
+            heapify(arr, n, largest);
+        }
+    }
+
+    static void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+
+            int pi = partition(arr, low, high);
+
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+        }
+    }
+
+    static int partition(int[] arr, int low, int high) {
+
+        int pivot = arr[high];
+
+        int i = (low - 1);
+
+        for (int j = low; j <= high - 1; j++) {
+
+            if (arr[j] < pivot) {
+
+                i++;
+                swap(arr, i, j);
+            }
+        }
+        swap(arr, i + 1, high);
+        return (i + 1);
+    }
+
+}
