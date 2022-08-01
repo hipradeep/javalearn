f5693b0db485005de1f93bed0a101013fa8896b1
pradeep <pradeepmaurya.cse@gmail.com>
2022-08-01 12:12:47 +0530

    Magic Square
    Signed-off-by: pradeep <pradeepmaurya.cse@gmail.com>

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # javalearn
-### Open 'src' folder for code
-# pradeep
+### File Structures
+#### [src]()
+#### - [basic java](https://github.com/hipradeep/javalearn/tree/master/src/basicjava)
+#### - [core java](https://github.com/hipradeep/javalearn/tree/master/src/corejava)
+#### - [advance java](https://github.com/hipradeep/javalearn/tree/master/src/advjava)
+#### - [dsa notes](https://github.com/hipradeep/javalearn/tree/master/src/dsanotes)
+
+# Stack
+
+# Queue
+# LinkedList
+
+
---
+++ src/dsanotes/datastructures/stacks/problems/BracketsMatching.java
@@ -0,0 +1,70 @@
+package dsanotes.datastructures.stacks.problems;
+
+public class BracketsMatching {
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("--Bracket checker--");
+        CharStackArray stack = new CharStackArray(10);
+        stack.balanceSymbol("[a+b{c+(e-f[p-q])}]");
+        stack.display();
+
+    }
+}
+
+class CharStackArray {
+    private char[] array;
+    private int top;
+    private int capacity;
+
+    public CharStackArray(int cap) {
+        capacity = cap;
+        array = new char[capacity];
+        top = -1;
+    }
+
+    public void push(char data) {
+        array[++top] = data;
+    }
+
+    public char pop() {
+        return array[top--];
+    }
+
+    public void display() {
+        for (int i = 0; i <= top; i++) {
+            System.out.print(array[i] + "->");
+        }
+    }
+
+    public char peek() throws Exception {
+        return array[top];
+    }
+
+    //Call this method by passing a string expression
+    public void balanceSymbol(String str) {
+        try {
+            char[] arr = str.toCharArray();
+            for (int i = 0; i < arr.length; i++) {
+                if (arr[i] == '[' || arr[i] == '{' || arr[i] == '(')
+                    push(arr[i]);
+                else if (arr[i] == '}' && peek() == '{')
+                    pop();
+                else if (arr[i] == ']' && peek() == '[')
+                    pop();
+                else if (arr[i] == ')' && peek() == '(')
+                    pop();
+            }
+            if (isEmpty()) {
+                System.out.println("String is balanced");
+            } else {
+                System.out.println("String is not balanced");
+            }
+        } catch (Exception e) {
+            System.out.println("String not balanced");
+        }
+    }
+
+    public boolean isEmpty() {
+        return (top == -1);
+    }
+}
